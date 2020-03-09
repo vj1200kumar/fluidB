0f18b6d3b03dd61b84a157861995467561aed129
Grigoriy Safronov <gvsafronov@yandex.ru>
2020-03-09 21:26:26 +0300

    Add files via upload

---
+++ fluwc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
+echo -e "Loading web-condole..."
+
+# Команда, которую нужно отследить
+./fluentbase-server > /dev/null &
+
+curl --data "set fleet truck3 point 33.4762 -112.10923" localhost:9470 
+
+curl localhost:9470/set+fleet+truck3+point+33.4762+-112.10923 
+
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+
+
+
