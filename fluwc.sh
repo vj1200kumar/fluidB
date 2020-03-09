ce359b776a1587099c46e97805b21d82da794489
Grigoriy Safronov <gvsafronov@yandex.ru>
2020-03-09 21:26:00 +0300

    Delete fluwc.sh

--- fluwc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./fluentbase-server > /dev/null &
-
-curl --data "set fleet truck3 point 33.4762 -112.10923" localhost:9470
-
-curl localhost:9470/set+fleet+truck3+point+33.4762+-112.10923
-
